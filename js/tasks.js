078a01dfb01d449cbab0aec20a5923e4399df4a8
Vitalij <vitalij.cernysiov@gmail.com>
2022-06-30 12:51:34 +0300

    first task

---
+++ js/tasks.js
@@ -0,0 +1,24 @@
+const a = 1;
+const b = 2;
+const c = 3;
+const u = 4;
+const y = 5;
+const d = "vit";
+const e = "cer";
+const z = "jur";
+
+console.log(a,b,c,d,e,z)
+
+console.clear()
+
+const f = 1.2456;
+console.log(f) 
+
+const i = "labas rytas Lietuva, stai atejau ir as"
+console.log(i)
+
+console.log(a+b+c+f)
+const p ="labas \n\ rytas \n\ Lietuva stai \n\ atejau ir as"
+console.log(p)
+
+console.log(a-b+c-u+y)
