2c63ee35927e580ae8ab14e82c12781a0b72ed2e
Vitalij <vitalij.cernysiov@gmail.com>
2022-07-03 15:43:28 +0300

    for

---
+++ js/for.js
@@ -0,0 +1,11 @@
+
+const marks = [20, 50, 10, 21, 48, 16]
+
+let suma = 0;
+for (let i = 0; i <= 5; i++)
+{const mark = marks[i];
+suma += mark;}
+
+console.log("suma",suma);
+console.log("kiekis",marks.length)
+console.log("vidurkis",suma/marks.length)
